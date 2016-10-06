903f3ff69fa76e7c51644d05bc566043ed0bc831
kjjgibson <kennethjjgibson@gmail.com>
2016-10-06 17:58:22 +1100

    Remove protect from forgery

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :web_hooks do
-    resources :semaphore_web_hooks, only: %i(create index)
+    resources :semaphore_web_hooks, only: %i(create)
   end
 
 end
