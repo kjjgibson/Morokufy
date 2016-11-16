4d76a6bb36aff6fb1d913201cd83b95cfe339dff
kjjgibson <kennethjjgibson@gmail.com>
2016-11-16 18:25:06 +1100

    debug: remove debug print

--- app/controllers/web_hooks/semaphore_web_hooks_controller.rb
+++ app/controllers/web_hooks/semaphore_web_hooks_controller.rb
@@ -3,8 +3,7 @@ module WebHooks
 
     def create
       status = :created
-      Rails.logger.info(request.env)
-      web_hook = WebHook.find_by_request_url(request.referrer)
+      web_hook = WebHook.find_by_request_url(request.referrer) #TODO: this header is not always set - we'll have to search the body of the request for a particular string (most webhooks should have a url containing the origin site anyways)
 
       if web_hook
         web_hook.run(params)
