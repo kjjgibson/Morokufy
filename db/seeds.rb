226a2295574ff366eb24b0fe712aa6f6eb4c9c60
kjjgibson <kennethjjgibson@gmail.com>
2016-12-29 16:11:32 +1100

    script: update achievements, rules, and webhooks scripts for achievements

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,36 +8,43 @@ require 'game_server/admin/request/player_external_event_request'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-web_hook = WebHook.new
-web_hook.name = 'Semaphore'
-web_hook.source_identifier = 'semaphore'
+WebHook.transaction do
+  web_hook = WebHook.find_or_initialize_by(name: 'Semaphore')
+  web_hook.source_identifier = 'semaphore'
 
-name_alias = web_hook.web_hook_alias_keys.build
-name_alias.alias_key = 'commit.author_name'
-name_alias.alias_type = Alias::AliasType::NAME
+  web_hook.web_hook_alias_keys.destroy_all
+  name_alias = web_hook.web_hook_alias_keys.build
+  name_alias.alias_key = 'commit.author_name'
+  name_alias.alias_type = Alias::AliasType::NAME
 
-email_alias = web_hook.web_hook_alias_keys.build
-email_alias.alias_key = 'commit.author_email'
-email_alias.alias_type = Alias::AliasType::EMAIL
+  email_alias = web_hook.web_hook_alias_keys.build
+  email_alias.alias_key = 'commit.author_email'
+  email_alias.alias_type = Alias::AliasType::EMAIL
 
-rule = web_hook.web_hook_rules.build
-rule.name = 'Post Build Success'
+  web_hook.web_hook_rules.destroy_all
+  rule = web_hook.web_hook_rules.build
+  rule.name = 'Post Build Success'
 
-predicate = rule.web_hook_predicates.build
-predicate.web_hook_key = 'result'
-predicate.expected_value = 'passed'
+  rule.web_hook_predicates.destroy_all
+  predicate = rule.web_hook_predicates.build
+  predicate.web_hook_key = 'result'
+  predicate.expected_value = 'passed'
 
-consequent = rule.web_hook_consequents.build
-consequent.event_name = GameServer::Admin::Request::PlayerExternalEventRequest::EventTypes::SEMAPHORE_BUILD_PASSED_EVENT
+  rule.web_hook_consequents.destroy_all
+  consequent = rule.web_hook_consequents.build
+  consequent.event_name = GameServer::Admin::Request::PlayerExternalEventRequest::EventTypes::SEMAPHORE_BUILD_PASSED_EVENT
 
-rule = web_hook.web_hook_rules.build
-rule.name = 'Post Build Failed'
+  rule = web_hook.web_hook_rules.build
+  rule.name = 'Post Build Failed'
 
-predicate = rule.web_hook_predicates.build
-predicate.web_hook_key = 'result'
-predicate.expected_value = 'failed'
+  rule.web_hook_predicates.destroy_all
+  predicate = rule.web_hook_predicates.build
+  predicate.web_hook_key = 'result'
+  predicate.expected_value = 'failed'
 
-consequent = rule.web_hook_consequents.build
-consequent.event_name = GameServer::Admin::Request::PlayerExternalEventRequest::EventTypes::SEMAPHORE_BUILD_FAILED_EVENT
+  rule.web_hook_consequents.destroy_all
+  consequent = rule.web_hook_consequents.build
+  consequent.event_name = GameServer::Admin::Request::PlayerExternalEventRequest::EventTypes::SEMAPHORE_BUILD_FAILED_EVENT
 
-web_hook.save!
+  web_hook.save!
+end
--- lib/scripts/create_achievements.rb
+++ lib/scripts/create_achievements.rb
@@ -6,5 +6,14 @@ def build_achievement(name, description, image_url)
 end
 
 Achievement.transaction do
-  build_achievement('Beginner Tester', 'Successfully build five Semaphore projects', 'https://pbs.twimg.com/profile_images/574917057721978881/fAhdRiwP.png')
+  build_achievement('Does it Work?', 'Successfully build your first Semaphore project', 'https://s3-ap-southeast-2.amazonaws.com/gameserver-morokufy/achievement_semaphore_passed_1.png')
+  build_achievement('Testing the Waters', 'Successfully build five Semaphore projects', 'https://s3-ap-southeast-2.amazonaws.com/gameserver-morokufy/achievement_semaphore_passed_5.png')
+  build_achievement('Testing Your Skill', 'Successfully build ten Semaphore projects', 'https://s3-ap-southeast-2.amazonaws.com/gameserver-morokufy/achievement_semaphore_passed_10.png')
+  build_achievement('A Tester\'s Courage', 'Successfully build twenty Semaphore projects', 'https://s3-ap-southeast-2.amazonaws.com/gameserver-morokufy/achievement_semaphore_passed_20.png')
+  build_achievement('Quality Assured', 'Successfully build forty Semaphore projects', 'https://s3-ap-southeast-2.amazonaws.com/gameserver-morokufy/achievement_semaphore_passed_40.png')
+  build_achievement('Can\'t Test This!', 'Successfully build eighty Semaphore projects', 'https://s3-ap-southeast-2.amazonaws.com/gameserver-morokufy/achievement_semaphore_passed_80.png')
+  build_achievement('Green for Miles', 'Successfully build two hundred Semaphore projects', 'https://s3-ap-southeast-2.amazonaws.com/gameserver-morokufy/achievement_semaphore_passed_200.png')
+
+  build_achievement('It Worked On MY Machine', 'First failed Semaphore project build', 'https://s3-ap-southeast-2.amazonaws.com/gameserver-morokufy/achievement_semaphore_failed_1.png')
+  build_achievement('Testing Your Patience', 'Ten failed Semaphore project builds', 'https://s3-ap-southeast-2.amazonaws.com/gameserver-morokufy/achievement_semaphore_failed_10.png')
 end
