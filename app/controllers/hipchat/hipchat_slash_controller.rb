162f39cd6248fef1946d6bb9aba5030fc5241120
Mathew Button <mat@moroku.com>
2017-01-12 18:19:22 +1100

    alias needs to be in array

--- app/controllers/hipchat/hipchat_slash_controller.rb
+++ app/controllers/hipchat/hipchat_slash_controller.rb
@@ -3,7 +3,7 @@ module Hipchat
 
     def create
       player_alias = params[:item][:message][:from][:name]
-      player = create_or_get_player(Alias.new(alias_value: player_alias, alias_type: Alias::AliasType::NAME))
+      player = create_or_get_player([Alias.new(alias_value: player_alias, alias_type: Alias::AliasType::NAME)])
 
       command = params[:item][:message][:message].gsub('/stats ', '')
 
