6fe2b31ad8378ad858c1120b4401e6081c924400
Mathew Button <mat@moroku.com>
2017-01-16 17:38:19 +1100

    God damn abstraction

--- app/controllers/hipchat/hipchat_slash_controller.rb
+++ app/controllers/hipchat/hipchat_slash_controller.rb
@@ -10,8 +10,14 @@ module Hipchat
 
       response = ""
       if player.present?
-        gs_player = get_game_server_player(player)
-        response = setup_player_stats_notification(gs_player)
+        gs_response = GameServer::Client::Request::PlayerRequest.new(morokufy_player.api_key, morokufy_player.shared_secret).get_player(morokufy_player.identifier)
+        if gs_response.is_success?
+          gs_player = gs_response.player
+          response = setup_player_stats_notification(gs_player)
+        else
+          Rails.logger.error("Could not get the Player on the GameServer: #{response.error_message}")
+        end
+
       else
         response = {
             notify: false,
